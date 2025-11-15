7cda2ccbf21a66c4b5376e466ae7f0aec9b93a22
TH3EXE <marcosdesinger98@gmail.com>
2025-11-15 19:22:46 -0300

    feat:

--- js/auth.js
+++ js/auth.js
@@ -1,43 +1,35 @@
 // js/auth.js
 
-// Espera a página (index.html) carregar
 document.addEventListener('DOMContentLoaded', () => {
     
-    // Pega os elementos do HTML
     const loginForm = document.getElementById('login-form');
     const usernameInput = document.getElementById('username');
     const passwordInput = document.getElementById('password');
     const errorMessage = document.getElementById('error-message');
 
-    // Se o usuário já está logado, manda ele direto para a página de busca
-    // (Também corrigido de 'app.html' para 'busca.html')
+    // Se já estiver logado, manda para a tela de apresentação
     if (sessionStorage.getItem('userRole')) {
-        window.location.href = 'busca.html';
+        // ATUALIZADO: Aponta para a nova página de apresentação
+        window.location.href = 'busca.html'; 
         return;
     }
 
-    // Evento de clique no botão "CONECTAR"
     loginForm.addEventListener('submit', (e) => {
-        e.preventDefault(); // Impede o recarregamento da página
+        e.preventDefault(); 
 
         const username = usernameInput.value;
         const password = passwordInput.value;
-
-        // --- LÓGICA DE AUTENTICAÇÃO "FALSA" ---
         
         let userRole = null;
 
         if (username === 'BATMAN' && password === 'BATMAN666') {
-            userRole = 'ADM'; // Define o papel como Administrador
+            userRole = 'ADM';
         } else if (username === 'OPERADOR' && password === '123') {
             userRole = 'OPERADOR';
         }
-        // Adicione mais operadores aqui...
-
-        // -----------------------------------------
+        // ... (outros usuários)
 
         if (userRole) {
-            // SUCESSO!
             errorMessage.textContent = 'CONECTANDO...';
             
             sessionStorage.setItem('userRole', userRole);
@@ -50,16 +42,12 @@ document.addEventListener('DOMContentLoaded', () => {
             loginCount++;
             localStorage.setItem('loginCount', loginCount);
 
-            // =========================================================
-            // CORREÇÃO ESTÁ AQUI
-            // O sistema agora redireciona para 'busca.html'
-            // =========================================================
+            // ATUALIZADO: Aponta para a nova página de apresentação
             window.location.href = 'busca.html';
 
         } else {
-            // FALHA!
             errorMessage.textContent = 'ERRO: Operador ou Senha inválidos.';
-            passwordInput.value = ''; // Limpa a senha
+            passwordInput.value = ''; 
         }
     });
 });
